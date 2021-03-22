db98cd5a53a4b3eec28c7a6ddfbfd153a65eb71f
João Schons <jvschons@gmail.com>
2021-03-22 13:18:45 -0300

    Criada a entidade Assignment; criado o contrato de interface para o repository da entidade

---
+++ AssignmentsManager.Core/Entities/Assignments/Assignment.cs
@@ -0,0 +1,10 @@
+﻿namespace AssignmentsManager.Core.Entities.Assignments
+{
+    public class Assignment
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public bool HasBeenDone { get; set; }
+    }
+}
---
+++ AssignmentsManager.Core/Entities/Assignments/IAssignmentRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AssignmentsManager.Core.Entities.Assignments
+{
+    public interface IAssignmentRepository
+    {
+        Task<Assignment> GetById(int id);
+        Task<List<Assignment>> GetAll();
+        Task<List<Assignment>> GetDone();
+        Task<List<Assignment>> GetNotDoneYet();
+        Task<int> CreateAssignment(Assignment assignment);
+        Task<int> UpdateAssignment(Assignment assignment);
+        Task<int> DeleteAssignment(Assignment assignment);
+    }
+}
