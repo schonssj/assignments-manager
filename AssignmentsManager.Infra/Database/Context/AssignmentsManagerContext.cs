51c6edb0e0542f0baa528e6ecc27f5ece8357b57
João Schons <jvschons@gmail.com>
2021-03-22 13:18:21 -0300

    Criada a classe de contexto de conexão com o banco de dados

---
+++ AssignmentsManager.Infra/Database/Context/AssignmentsManagerContext.cs
@@ -0,0 +1,14 @@
+﻿using AssignmentsManager.Core.Entities.Assignments;
+using Microsoft.EntityFrameworkCore;
+
+namespace AssignmentsManager.Infra.Database.Context
+{
+    public class AssignmentsManagerContext : DbContext
+    {
+        public DbSet<Assignment> Assignment { get; set; }
+
+        public AssignmentsManagerContext(DbContextOptions<AssignmentsManagerContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) => modelBuilder.Entity<Assignment>().HasKey(t => t.Id);
+    }
+}
