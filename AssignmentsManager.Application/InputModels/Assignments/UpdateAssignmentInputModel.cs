54bbcfe517812930e8f4e0f6eb467ce964d0230f
João Schons <jvschons@gmail.com>
2021-03-22 13:19:35 -0300

    Criados os InputModels para os dados de entrada de criação e atualização de um objeto da entidade Assignment

---
+++ AssignmentsManager.Application/InputModels/Assignments/UpdateAssignmentInputModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AssignmentsManager.Application.InputModels.Assignments
+{
+    public class UpdateAssignmentInputModel
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public bool HasBeenDone { get; set; }
+    }
+}
