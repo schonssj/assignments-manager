05ccb9082ff01175db5f019616a3b664aa6da507
João Schons <jvschons@gmail.com>
2021-03-22 13:19:10 -0300

    Criado o ViewModel para exibição dos dados da entidade Assignment

---
+++ AssignmentsManager.Application/ViewModels/Assignments/AssignmentViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace AssignmentsManager.Application.ViewModels.Assignments
+{
+    public class AssignmentViewModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public bool HasBeenDone { get; set; }
+    }
+}
